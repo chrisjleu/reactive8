9f5cc8e547f569d000172a21af8d0d9b2f2ab952
Christopher Leung <christopher.leung@asml.com>
2017-12-30 11:36:45 +0100

    Basic setup of client and server components

---
+++ README.md
@@ -0,0 +1,17 @@
+# Reactive8
+An [opnionated](https://stackoverflow.com/questions/802050/what-is-opinionated-software#802064) starter kit for building reactive web applications with microservices.
+
+### What you get
+1. [Vert.x](http://vertx.io) on the backend.
+1. [SPA](https://en.wikipedia.org/wiki/Single-page_application) with Angular on the frontend.
+1. Maven to build, package and run as an executable (fat) JAR.
+
+### Coming soon
+1. Reactive frontend with websockets
+
+## Getting started
+1. Install Java 8.
+1. Checkout the code and `cd` to the checkout directory.
+1. `./mvnw clean package`
+1. `java -jar server/target/server-1.0.0-SNAPSHOT.jar`
+1. [localhost:8080](http://localhost:8080)
---
+++ server/src/main/java/io/reactive8/server/MainVerticle.java
@@ -0,0 +1,28 @@
+package io.reactive8.server;
+
+
+import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.reactivex.ext.web.Router;
+import io.vertx.reactivex.ext.web.handler.StaticHandler;
+
+
+public class MainVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() throws Exception {
+        Router router = Router.router(vertx);
+
+        // Setup serving of static content
+        StaticHandler staticHandler = StaticHandler.create().setIndexPage("client/index.html");
+
+        router.route("/assets/*").handler(staticHandler);
+        router.route("/").handler(request -> request.reroute("/assets/"));
+
+        vertx.createHttpServer()
+            .requestHandler(router::accept)
+            .rxListen(8080)
+            .doOnSuccess(result -> System.out.println("HTTP server started on port 8080"))
+            .doOnError(e -> System.out.println("failed to start owing to " + e.getMessage()))
+            .subscribe();
+    }
+}
